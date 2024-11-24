525f1ad865e35b2d217e5f6002fc612c6d0c49c7
Nikhil Saji <nikhilsaji200@gmail.com>
2024-11-24 16:13:09 -0500

    Final commit

--- graph.py
+++ graph.py
@@ -4,21 +4,21 @@ import seaborn as sns
 import pandas as pd
 
 # multi = [0.432,1.466,3.438,18.9,77.052,386.267]
-loop = [0.18,0.96,2.8,350.29,847.21]
-spark = [0.49,0.35,0.31,0.38,0.4]
-sentences = [1000, 5000, 10000, 50000, 100000]
+loop = [0.0037 ,0.0288 ,0.6647 ,3.2473]
+spark = [0.0048 ,0.0037 ,0.0106 ,0.0217 ]
+birds = [200, 1000, 5000, 10000]
 
-plt.plot(sentences,loop, label='Non-Spark',marker='o')
+plt.plot(birds,loop, label='Non-Spark',marker='o')
 # plt.plot(sentences,multi, label='Multi-Processing',marker='*')
-plt.plot(sentences,spark, label='Spark',marker='^')
+plt.plot(birds,spark, label='Spark',marker='^')
 plt.legend()
-plt.xlabel('Number of Scentences')
+plt.xlabel('Number of Birds')
 plt.ylabel('Execution Time (seconds)')
 plt.xscale('linear')
 plt.yscale('log')
 plt.grid(True, which='both', linestyle='-.', linewidth=0.5)
 plt.legend(fontsize=12)
-plt.xticks(sentences, fontsize=10)
+plt.xticks(birds, fontsize=10)
 plt.yticks(fontsize=10)
 
 # Show the plot
